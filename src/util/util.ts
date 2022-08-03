88b3afeb23a48a21b92bd1417aa35d190ef6814f
ayush-lal <ayush.lal@outlook.com>
2022-08-03 16:15:27 +1000

    refactored and finalised file

--- src/util/util.ts
+++ src/util/util.ts
@@ -4,6 +4,11 @@ export async function getData() {
   try {
     const { data } = await axios.get(
       'https://api.github.com/users/ayush-lal/repos',
+      {
+        headers: {
+          Authorization: `${import.meta.env.VITE_GH_APIKEY}`,
+        },
+      },
     );
 
     const response = data.map((i: any) => i.languages_url);
@@ -11,7 +16,7 @@ export async function getData() {
     const repoLanguage = await getRepoLanguage(response);
 
     const flattenedAndReduced = repoLanguage.reduce((prev, current) => {
-      Object.keys(prev).forEach((key, index) => {
+      Object.keys(prev).forEach(key => {
         if (current[key]) {
           prev[key] += current[key];
           delete current[key];
@@ -29,27 +34,25 @@ export async function getData() {
       }),
     );
 
-    const test = flattenedAndReducedToArray.map((i: any) => {
-      //   console.log(i[Object.keys(i)[0]]);
-      //   console.log(Object.keys(i)[0]);
-      //   console.log(i[Object.keys(i)[0]]);
-
+    return flattenedAndReducedToArray.map((i: any) => {
       return {
         label: Object.keys(i)[0],
         value: i[Object.keys(i)[0]],
       };
     });
-
-    return test;
   } catch (error) {
-    throw error;
+    console.log(error);
   }
 }
 
 async function getRepoLanguage(array: string[]) {
   let response = await Promise.all(
     array.map(async repo => {
-      const { data } = await axios.get(repo);
+      const { data } = await axios.get(repo, {
+        headers: {
+          Authorization: `${import.meta.env.VITE_GH_APIKEY}`,
+        },
+      });
       return data;
     }),
   );
@@ -57,4 +60,25 @@ async function getRepoLanguage(array: string[]) {
   return response;
 }
 
-export const colours = ['#FF6384', '#36A2EB', '#FFCE56'];
+export const colours = [
+  '#a5c9ff',
+  '#f8b380',
+  '#d692ce',
+  '#b7fcae',
+  '#8efabb',
+  '#808de6',
+  '#f895bf',
+  '#9caac8',
+  '#9bddff',
+  '#8cc69e',
+  '#a195aa',
+  '#93fffd',
+  '#e3d5dd',
+  '#ff8cf0',
+  '#cff280',
+  '#87adc0',
+  '#899880',
+  '#f7fd80',
+  '#eca087',
+  '#b78aff',
+];
